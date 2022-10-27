53eb3704771ad15b6e8c3e2ad6f0d0d0c39e1f6e
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-10-28 00:38:04 +0600

    login page content

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,11 @@
             </a>
         </x-slot>
 
+        <div class="text-center">
+            <h2>Welcome back</h2>
+            <p>Continue with the email address you use to sign in</p>
+        </div>
+
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -14,18 +19,14 @@
 
             <!-- Email Address -->
             <div>
-                <x-input-label for="email" :value="__('Email')" />
-
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-text-input id="email" placeholder="Email Address" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
 
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
-
-                <x-text-input id="password" class="block mt-1 w-full"
+                <x-text-input id="password" placeholder="Password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
                                 required autocomplete="current-password" />
@@ -34,24 +35,28 @@
             </div>
 
             <!-- Remember Me -->
-            <div class="block mt-4">
+            <div class="flex items-center justify-between mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
-            </div>
 
-            <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
+            </div>
 
-                <x-primary-button class="ml-3">
+            <div class="mt-4">
+                <x-primary-button class="w-full block text-center">
                     {{ __('Log in') }}
                 </x-primary-button>
             </div>
         </form>
+
+        <div class="mt-2">
+            <span class="">Don't have an account? <a href="{{ route('register') }}" class="">Sign up for free</a></span>
+        </div>
     </x-auth-card>
 </x-guest-layout>
--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,9 +1,14 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
-        {{ $logo }}
-    </div>
+<div class="flex">
+    <div class="min-h-screen w-1/3 ">
+        <div>
+            {{ $logo }}
+        </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+        </div>
+    </div>
+    <div class="w w-2/3 bg-black min-h-screen">
+
     </div>
 </div>
