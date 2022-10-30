a3f79468568b7ab86c6fe78ea9b589acdba72ebd
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-10-30 16:58:15 +0600

    laravel mix active

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,12 @@
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('./backend/css/custom.css') }}" />
+        <script src="{{ asset('js/app.js') }}"></script>
+        {{-- custom js --}}
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="{{ asset('backend/js/custom.js') }}"></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
